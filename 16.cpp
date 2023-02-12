2983f40b4edb45d1a8691753e6288e0f2b782e6d
mtlee26 <125125121+mtlee26@users.noreply.github.com>
2023-02-12 20:54:07 +0700

    Create 16.cpp

---
+++ 16.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a == b && b == c) cout << “ true “ ;
+    else cout << “false”;
+    return 0;
+}
