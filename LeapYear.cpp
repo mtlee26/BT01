61cdb253d820dae2d8dbc070ab9ab654cf6abc9f
mtlee26 <125125121+mtlee26@users.noreply.github.com>
2023-02-12 20:54:37 +0700

    Create LeapYear.cpp

---
+++ LeapYear.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x ;
+    if (x % 4 == 0) cout << “ true “ ;
+    else cout << “false”;
+    return 0;
+}
